10e373d54602e9e054ac1b2e77ff4296ee72320e
SteelOnion <steelonion@foxmail.com>
2023-08-17 23:22:09 +0800

    添加TCP框架

---
+++ SteelOnion.ProtocolStack/Protocol/ProtocolTCP.cs
@@ -0,0 +1,92 @@
+﻿using PacketDotNet;
+using SteelOnion.ProtocolStack.ProtocolArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteelOnion.ProtocolStack.Protocol
+{
+    internal class ProtocolTCP : ProtocolBase<TcpPacket, ProtocolIPArgs>
+    {
+
+        private Dictionary<int, SimulatedTcpClient> _ports;
+
+        public ProtocolTCP(ProtocolStackConfig config) : base(config)
+        {
+            _ports = new Dictionary<int, SimulatedTcpClient>();
+        }
+
+        public override string ProtocolName => "TCP";
+
+        /// <summary>
+        /// 发起一个tcp连接请求
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="remote"></param>
+        /// <returns></returns>
+        private bool Connect(int port,IPEndPoint remote)
+        {
+            if (_ports.TryGetValue(port,out var client))
+            {
+                //开始三次握手
+                TcpPacket packet = new TcpPacket((ushort)port, (ushort)remote.Port);
+                packet.SequenceNumber = client.Seq;
+                packet.Synchronize=true;
+                SendPacket?.Invoke(this, new ProtocolIPArgs(packet, remote.Address, null));
+                //block
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal SimulatedTcpClient CreateClient(int port)
+        {
+            if (port < 0) { throw new ArgumentOutOfRangeException("port"); }
+            if (port > 65534) { throw new ArgumentOutOfRangeException("port"); }
+            if (_ports.ContainsKey(port)) { throw new InvalidOperationException("port has used"); }
+            return _ports[port] = new SimulatedTcpClient(port, RemoveClient, ClientSendPacket);
+        }
+
+        private bool ClientSendPacket(int arg1, byte[] arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveClient(int port)
+        {
+            //开始四次挥手
+            _ports.Remove(port);
+        }
+
+        public override event EventHandler<ProtocolIPArgs>? SendPacket;
+
+        public override void ReceivePacket(TcpPacket packet)
+        {
+            if (_ports.TryGetValue(packet.DestinationPort, out SimulatedTcpClient? client))
+            {
+                //服务器端确认报文
+                if (packet.Synchronize && packet.Acknowledgment)
+                {
+                    //此处应当验证
+                    //发送应答
+                    var retPacket = new TcpPacket((ushort)client.Port, (ushort)client.Remote.Port);
+                    retPacket.Acknowledgment = true;
+                    retPacket.AcknowledgmentNumber = packet.SequenceNumber + 1;
+                    SendPacket?.Invoke(this, new ProtocolIPArgs(packet, client.Remote.Address, null));
+                    //客户端进入可通讯
+                    client.Established = true;
+                }
+                else
+                {
+                    client.EnqueuePacket(packet);
+                }
+            }
+        }
+    }
+}
---
+++ SteelOnion.ProtocolStack/SimulatedTcpClient.cs
@@ -0,0 +1,93 @@
+﻿using PacketDotNet;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteelOnion.ProtocolStack
+{
+    internal class SimulatedTcpClient
+    {
+        static Random Random = new Random(DateTime.Now.Millisecond);
+
+        /// <summary>
+        /// 对端地址
+        /// </summary>
+        public IPEndPoint Remote { get; }
+
+        /// <summary>
+        /// 检查ACK是否正确
+        /// </summary>
+        /// <param name="ack"></param>
+        /// <returns></returns>
+        internal bool CheckAck(uint ack)
+        {
+            return ack == _seq + 1;
+        }
+
+        private uint _seq;
+        internal uint Seq => _seq++;
+
+        internal bool Established { get; set; }
+
+        /// <summary>
+        /// 当前实例已经被释放
+        /// </summary>
+        internal Action<int> Disposed;
+
+        internal Func<int, byte[], bool> SendFunc;
+
+        internal BlockingCollection<TcpPacket> _packets;
+
+        public SimulatedTcpClient(int port, Action<int> disposed, Func<int, byte[], bool> sendFunc)
+        {
+            Established = false;
+            Port = port;
+            Disposed = disposed;
+            SendFunc = sendFunc;
+            _packets = new BlockingCollection<TcpPacket>();
+            _seq = (uint)Random.Next();
+        }
+
+        internal void EnqueuePacket(TcpPacket packet)
+        {
+            _packets.Add(packet);
+        }
+
+        /// <summary>
+        /// 端口号
+        /// </summary>
+        public int Port { get; }
+
+        /// <summary>
+        /// 发送数据流
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="remote"></param>
+        /// <returns></returns>
+        public bool Send(byte[] data)
+        {
+            return SendFunc(Port, data);
+        }
+
+        /// <summary>
+        /// 读取数据流
+        /// </summary>
+        /// <param name="remote"></param>
+        /// <returns></returns>
+        public byte[] Read()
+        {
+            var packet = _packets.Take();
+            return packet.PayloadData;
+        }
+
+        public void Dispose()
+        {
+            _packets.CompleteAdding();
+            Disposed?.Invoke(Port);
+        }
+    }
+}
